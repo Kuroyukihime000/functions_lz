1cbd6c62197e68a9fff05389cba098343eb09398
Kuroyukihime000 <mishutka2282@tutamail.com>
2024-12-05 15:41:17 +0300

    готово

--- circle.py
+++ circle.py
@@ -1,14 +1,15 @@
-print("Введите радиус: ") #Ввод
-radius=input()
-radius=int(radius)
 def pl(a):
     pl=a*a*3.14
     return pl
-
  
 def dl(a):        #Функция поиска длины
     dl=2*a*3.14
     return dl
+
+
+print("Введите радиус: ") #Ввод
+radius=input()
+radius=int(radius)
 
 pl=pl(radius)             #Использовние функций
 dl=dl(radius)
--- temperrature.py
+++ temperrature.py
@@ -2,10 +2,10 @@ print("Введите температуру")
 temper=input()
 temper=float(temper)
 def temp(a):
-    temp = a * 1,8 + 32
-    return temp
-temp = temp(temper)
-print('Температура в форенгейтах ',temp)
+    lol = a * 1.8 + 32
+    return lol
+res = temp(temper)
+print('Температура в форенгейтах ', res)
 
 
     
